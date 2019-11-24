2e4731a35e1488221f163d990423b813b7ef4fe7
Christiana <kristiana.095@gmail.com>
2019-11-24 14:35:29 +0000

    Spring boot setup

---
+++ src/main/java/io/turntabl/TCMSProjects/Service/TcmsProjectsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.TCMSProjects.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmsProjectsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmsProjectsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/TCMSProjects/Service/TcmsProjectsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.TCMSProjects.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmsProjectsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
